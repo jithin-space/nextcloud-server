bea7e450e6e6151e377a3c266625669baf8cc5f1
provokateurin <kate@provokateurin.de>
2024-01-15 15:27:20 +0100

    fixup! feat(AppFramework): Add Route attribute

--- lib/public/AppFramework/Http/Attribute/Route.php
+++ lib/public/AppFramework/Http/Attribute/Route.php
@@ -25,11 +25,25 @@ namespace OCP\AppFramework\Http\Attribute;
 
 use Attribute;
 
+/**
+ * This attribute can be used to define routes on controller methods.
+ *
+ * It works in addition to the traditional routes.php method and has the same parameters
+ * (except for the `name` parameter which is not needed).
+ *
+ * @since 29.0.0
+ */
 #[Attribute(Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
 class Route {
 
+	/**
+	 * Corresponds to the `ocs` key in routes.php
+	 */
 	public const TYPE_OCS = 'ocs';
 
+	/**
+	 * Corresponds to the `routes` key in routes.php
+	 */
 	public const TYPE_INDEX = 'routes';
 
 	/**
@@ -42,6 +56,7 @@ class Route {
 	 * @param ?array<string, mixed> $defaults Array of default values mapped to the path parameters.
 	 * @param ?string $root Custom root. For OCS all apps are allowed, but for index.php only some can use it.
 	 * @param ?string $postfix Postfix for the route name.
+	 * @since 29.0.0
 	 */
 	public function __construct(
 		protected string $type,
@@ -63,6 +78,7 @@ class Route {
 	 *     root?: string,
 	 *     postfix?: string,
 	 * }
+	 * @since 29.0.0
 	 */
 	public function toArray() {
 		$route = [
@@ -86,30 +102,51 @@ class Route {
 		return $route;
 	}
 
+	/**
+	 * @since 29.0.0
+	 */
 	public function getType(): string {
 		return $this->type;
 	}
 
+	/**
+	 * @since 29.0.0
+	 */
 	public function getVerb(): ?string {
 		return $this->verb;
 	}
 
+	/**
+	 * @since 29.0.0
+	 */
 	public function getUrl(): string {
 		return $this->url;
 	}
 
+	/**
+	 * @since 29.0.0
+	 */
 	public function getRequirements(): ?array {
 		return $this->requirements;
 	}
 
+	/**
+	 * @since 29.0.0
+	 */
 	public function getDefaults(): ?array {
 		return $this->defaults;
 	}
 
+	/**
+	 * @since 29.0.0
+	 */
 	public function getRoot(): ?string {
 		return $this->root;
 	}
 
+	/**
+	 * @since 29.0.0
+	 */
 	public function getPostfix(): ?string {
 		return $this->postfix;
 	}
